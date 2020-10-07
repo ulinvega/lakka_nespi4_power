f65416358f3f36edc295f18fc69ef071640961d2
ulinvega <33462793+ulinvega@users.noreply.github.com>
2020-10-07 00:12:58 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+SourcePath=https://raw.githubusercontent.com/RetroFlag/retroflag-picase/master
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Please execute script as root." 
+   exit 1
+fi
+
+File=/boot/config.txt
+wget -O  "/boot/overlays/RetroFlag_pw_io.dtbo" "$SourcePath/RetroFlag_pw_io.dtbo"
+if grep -q "RetroFlag_pw_io" "$File";
+	then
+		sed -i '/RetroFlag_pw_io/c dtoverlay=RetroFlag_pw_io.dtbo' $File 
+		echo "PW IO fix."
+	else
+		echo "dtoverlay=RetroFlag_pw_io.dtbo" >> $File
+		echo "PW IO enabled."
+fi
+if grep -q "enable_uart" "$File";
+	then
+		sed -i '/enable_uart/c enable_uart=1' $File 
+		echo "UART fix."
+	else
+		echo "enable_uart=1" >> $File
+		echo "UART enabled."
+fi
+
+mkdir "/opt/RetroFlag"
+script=/opt/RetroFlag/SafeShutdown.py
+wget -O $script "$SourcePath/SafeShutdown.py"
+
+RC=/etc/rc.local
+
+if grep -q "python $script &" "$RC";
+	then
+		echo "File $RC already configured. Doing nothing."
+	else
+		sed -i -e "s/^exit 0/python \/opt\/RetroFlag\/SafeShutdown.py \&\n&/g" "$RC"
+		echo "File /etc/rc.local configured."
+fi
+
+echo "RetroFlag Pi Case installation done. Will now reboot after 3 seconds."
+sleep 3
+reboot
